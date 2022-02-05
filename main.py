8372f133b06cee60ab3e0fa0a9d3ef4836aee387
nabil <merchant.nabil@gmail.com>
2022-02-05 12:42:31 -0600

    Fixing button appearance and background color.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class CameraScreen(Screen):
         """
         Starts the camera and changes the button text.
         """
+        self.ids.camera.opacity = 1
         self.ids.camera.play = True
         self.ids.camera_button.text = 'Stop Camera'
         self.ids.camera.texture = self.ids.camera._camera.texture
@@ -23,6 +24,7 @@ class CameraScreen(Screen):
         """
         Stops the camera and changes the button text.
         """
+        self.ids.camera.opacity = 0
         self.ids.camera.play = False
         self.ids.camera_button.text = 'Start Camera'
         self.ids.camera.texture = None
